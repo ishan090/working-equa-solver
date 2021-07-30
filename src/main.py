a2c545f733f4b5b9613393dc4bca2dd8c17c609c
snehashish <snehashish.laskar@sahyadrischool.org>
2021-07-30 23:28:15 +0530

    started the project on 30-7-2021

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# This is the main file of this project
+# You should not use delete this!
+
+# Imports
+
+# Authors: Snehashish Laskar and Ishan Kashyap
+# Date: 1st August 2021
+# Contact: organizationpythonprogramming.gmail.com
+
+from models import *
---
+++ src/models.py
@@ -0,0 +1,21 @@
+# This file is a file that contains all the classes and models for the main.py file
+# This file is very important so please dont delete it
+
+
+# Imports
+
+
+# Authors: Snehashish Laskar and Ishan Kashyap
+# Date: 1st August 2021
+# Contact: organizationpythonprogramming.gmail.com
+
+
+class equation:
+
+	def __init__(self, string):
+		pass
+
+class Term:
+
+	def __init__(self):
+		pass
